4c2c0ece3a39a1a32d0a21fa3808cbb4adf62ef3
developineta <77379211+developineta@users.noreply.github.com>
2021-11-17 00:03:11 +0200

    Addition of movie data
    Movie lists in assets and Swift files

--- TrackList/Model/DataManager.swift
+++ TrackList/Model/DataManager.swift
@@ -56,48 +56,29 @@ class DataManager {
             "YG - Stop Snitching",
         ]
     
-    var movie = [
-            "Carly Rae Japsen",
-            "Dj Khaled",
-            "Injury Reserve",
-            "Jonas Brothers",
-            "Joyner Lucas",
-            "Maluma",
-            "Never Young",
-            "Rammstein",
-            "Skepta",
-            "Skillet",
-            "Tylor the Creator",
-            "YG",
+    var filmName = [
+            "Back To The Future",
+            "Batman",
+            "Da Vinci",
+            "Matrix",
+            "Star Wars",
+            "The Mask"
         ]
     
     var year = [
-            "Dedicated",
-            "father of",
-            "Jailbrake",
-            "Happiness Begins ",
-            "adhd",
-            "11-11",
-            "LOSER",
-            "rammstein",
-            "Ignorance Is Bliss",
-            "Victory",
-            "IGOR",
-            "Stop Snitching",
+            "1985",
+            "1966",
+            "2006",
+            "1999",
+            "1977",
+            "1994"
         ]
     
     var poster = [
-            "Carly Rae Japsen - Dedicated",
-            "Dj Khaled - father of",
-            "Injury Reserve - Jailbrake",
-            "Jonas Brothers - Happiness Begins ",
-            "Joyner Lucas - adhd",
-            "Maluma - 11-11",
-            "Never Young - LOSER",
-            "Rammstein - rammstein",
-            "Skepta - Ignorance Is Bliss",
-            "Skillet - Victory",
-            "Tylor the Creator - IGOR",
-            "YG - Stop Snitching",
+            "Back To The Future - 1985",
+            "Batman - 1966",
+            "Da Vinci Code - 2006",
+            "Star Wars - 1977",
+            "The Mask - 1994"
         ]
 }
